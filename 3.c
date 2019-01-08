aae7df82d1a26f38a4c8e227c8271c3a5a29c1cc
MarkStav <mark.gurianov@gmail.com>
2019-01-08 16:17:08 +0300

    Задание №3

---
+++ 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, const char* argv[]) {
+    int i = 1;
+    int sum = 0;
+    printf("Введите последовательность чисел:\n");
+    while (i <= 10){
+        int n = 0;
+        scanf("%d",&n);
+        sum += n;
+        i ++;
+    }
+    printf("Среднее арифметическое введённых чисел = %f",(double)sum / 10.0 );
+    return 0;
+}
